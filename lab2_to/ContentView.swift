8b75ff886d7f413818140ba07620534238422798
user <user@account.com>
2024-04-27 17:03:19 +0200

    Initial Commit

---
+++ lab2_to/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  lab2_to
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ lab2_to/lab2_toApp.swift
@@ -0,0 +1,17 @@
+//
+//  lab2_toApp.swift
+//  lab2_to
+//
+//  Created by student on 27/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct lab2_toApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
